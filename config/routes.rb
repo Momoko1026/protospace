bc095fe66313ff6986f48620e748320ba1b47a62
筒井桃子 <momokotsutsui1026@gmail.com>
2016-01-23 18:42:02 +0900

    updateの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
-  resources :prototypes, only: [:show, :new, :edit]
+  resources :users, only: [:show, :edit, :update]
+  resources :prototypes, only: [:show, :new, :edit, :update]
   reources :tags, only: [:index :show]
   root 'prototypes#index'
 end
