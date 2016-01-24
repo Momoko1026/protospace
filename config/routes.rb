29e79146f255b3ae0997446d2c0296fa37f93998
筒井桃子 <momokotsutsui1026@gmail.com>
2016-01-25 00:34:50 +0900

    ファイルの置き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
-  resources :prototypes, only: [:show, :new, :edit, :update]
-  reources :tags, only: [:index :show]
-  root 'prototypes#index'
+  resources :proto, only: [:show, :new, :edit, :update]
+  resources :tags, only: [:index, :show]
+  root 'proto#index'
 end
