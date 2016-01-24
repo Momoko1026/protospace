3424b4fd111827288ca94e63cc1b7dbd9d762e1d
筒井桃子 <momokotsutsui1026@gmail.com>
2016-01-25 00:48:31 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
-  resources :proto, only: [:show, :new, :edit, :update]
+  resources :proto, only: [:index, :show, :new, :edit, :update]
   resources :tags, only: [:index, :show]
-  root 'proto#index'
+  root 'top#index'
 end
